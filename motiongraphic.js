91822f040b96bbfa046549f3c7befe3ee72ff73d
Velmurugan R <cenavel2@gmail.com>
2018-10-19 10:11:00 +0530

    Create motiongraphic.js

---
+++ motiongraphic.js
@@ -0,0 +1,11 @@
+c = document.getElementById("MGraphic");
+var canvas = document.createElement('canvas');
+canvas.id = "CCcanvas";
+canvas.style.width = "100%";
+canvas.style.height = "100%";
+c.appendChild(canvas);
+ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.arc(100,75,50,0*Math.PI,2*Math.PI);
+ctx.lineWidth = 10;
+ctx.stroke();
