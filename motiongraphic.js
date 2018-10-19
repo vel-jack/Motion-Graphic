6567704716273e8b33d6efcb19ef228c071b6bcd
Velmurugan R <cenavel2@gmail.com>
2018-10-19 10:16:37 +0530

    Update motiongraphic.js

--- motiongraphic.js
+++ motiongraphic.js
@@ -1,11 +1,13 @@
-c = document.getElementById("MGraphic");
-var canvas = document.createElement('canvas');
-canvas.id = "CCcanvas";
-canvas.style.width = "100%";
-canvas.style.height = "100%";
-c.appendChild(canvas);
-ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(100,75,50,0*Math.PI,2*Math.PI);
-ctx.lineWidth = 10;
-ctx.stroke();
+function MotionGrapic(){
+  var mG = document.getElementById("MGraphic");
+  var canvas = document.createElement('canvas');
+  canvas.id = "CCcanvas";
+  canvas.style.width = "100%";
+  canvas.style.height = "100%";
+  mG.appendChild(canvas);
+  ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(100,75,50,0*Math.PI,2*Math.PI);
+  ctx.lineWidth = 10;
+  ctx.stroke();
+}
